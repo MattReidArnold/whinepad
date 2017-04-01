023b97943aae90d5a5fb51e02acb7aa332a0e565
Matt Arnold <mattreidarnold@gmail.com>
2017-04-01 17:22:29 -0600

    Added discovery page

---
+++ js/source/discover.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import Logo from './components/Logo';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div style={{ padding: '20px'}}>
+        <h1>Component Discoverer</h1>
+
+        <h2>Logo</h2>
+        <div style={{ display: 'inline-block', background: 'purple'}}>
+            <Logo />
+        </div>
+    </div>
+    ,document.getElementById('pad'));
